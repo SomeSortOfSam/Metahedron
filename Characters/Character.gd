fa7994b2c0e0216cc6da1224abbe8ac3003565ed
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-06-09 08:42:27 -0500

    Map Logic and Test Map Added
    Following https://www.gdquest.com/tutorial/godot/2d/tactical-rpg-movement
    up to lesson 1

---
+++ Characters/Character.gd
@@ -0,0 +1,6 @@
+extends KinematicBody2D
+class_name Character
+
+export(int) var move_speed = 1
+
+var TILE_SIZE = 80
---
+++ Maps/Map.gd
@@ -0,0 +1,25 @@
+extends Resource
+class_name Map
+
+# map size in tiles
+var size : Vector2
+var cell_size : Vector2
+
+var _half_cell_size = cell_size / 2
+
+func map_to_world_space(map_point : Vector2) -> Vector2 :
+	return map_point * cell_size + _half_cell_size
+
+func world_to_map_space(world_point : Vector2) -> Vector2 :
+	return (world_point / cell_size).floor()
+
+func is_within_bounds(map_point : Vector2) -> bool :
+	return map_point.x >= 0 && map_point.x < size.x && map_point.y >= 0 && map_point.y < size.y
+
+func clamp(map_point : Vector2) -> Vector2:
+	map_point.x = clamp(map_point.x,0,size.x)
+	map_point.y = clamp(map_point.y,0,size.y)
+	return map_point
+
+func map_to_map_index(map_point : Vector2) -> int :
+	return int(map_point.x + size.x * map_point.y)
