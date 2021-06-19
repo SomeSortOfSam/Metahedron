3e9f5607f292282ebce4d466b10fa02a4a791ede
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-06-19 10:28:56 -0500

    Install WAT in new project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Metaheadron Prototypes
- 
--- UnitOverlay.gd
+++
@@ -1,9 +0,0 @@
-extends TileMap
-class_name UnitOverlay
-
-export var map : Resource
-
-func draw(cells: Array):
-	clear()
-	for cell in cells:
-		set_cellv(cell + map.map_rect.position, 0)
