a1efda3a99b5be6bfea08c102df11a5303edab07
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-06-20 00:14:53 -0500

    refactor conversion tests

--- Map/Map.gd
+++ Map/Map.gd
@@ -9,7 +9,7 @@ func _init(tile_map : TileMap = TileMap.new()):
 	rect = tile_map.get_used_rect()
 
 func world_to_map(world_point : Vector2) -> Vector2:
-	return world_point/tile_map.cell_size - rect.position
+	return (((world_point - tile_map.position)/(tile_map.cell_size * tile_map.scale)) - rect.position).floor()
 
 func map_to_world(map_point : Vector2) -> Vector2:
-	return (map_point + rect.position) * tile_map.cell_size
+	return ((map_point + rect.position)*(tile_map.cell_size * tile_map.scale)) + tile_map.position + tile_map.cell_size/2
--- tests/map.test.gd
+++ tests/map.test.gd
@@ -1,5 +1,7 @@
 extends WAT.Test
 
+var size := Vector2.ONE * 4
+
 func test_empty_map_tests():
 	describe("New empty Map")
 	
@@ -10,8 +12,6 @@ func test_empty_map_tests():
 	asserts.is_equal(map.rect.size,Vector2.ZERO,"map size is zero")
 
 func test_3x3_map_tests():
-	var size := Vector2.ONE * 3
-	
 	describe("New " + str(size) + " Map")
 	var map := initialize_full_tilemap(size)
 	
@@ -19,45 +19,47 @@ func test_3x3_map_tests():
 	asserts.is_not_null(map.rect, "Map has rect")
 	asserts.is_equal(map.rect.size,size,"map size is " + str(size))
 
-func test_unscaled_untransformed_conversions():
-	describe("Unscaled, untransformed convertions")
-	
-	var map := initialize_full_tilemap(Vector2.ONE * 5)
-	
-	for x in map.rect.size.x:
-		for y in map.rect.size.y:
-			asserts.is_equal(map.world_to_map(Vector2(x,y)),Vector2(x,y),"World to Map convertion at " + str(Vector2(x,y)) + " returns same vector")
-			asserts.is_equal(map.map_to_world(Vector2(x,y)),Vector2(x,y),"Map to World convertion at " + str(Vector2(x,y)) + " returns same vector")
+func test_conversions():
+	var params := ["size","cell_size","cell_transform","object_size","object_transform"]
+	var default := [size, Vector2.ONE, Vector2.ZERO, Vector2.ONE, Vector2.ZERO]
+	parameters(generate_convertion_paramaters(params, default))
+	describe(generate_convertion_description(params, default))
+	run_conversion_tests(generate_convertion_map())
 
-func test_scaled_untransformed_conversions():
-	describe("Scaled x3, untransformed convertions")
-	
-	var map := initialize_full_tilemap(Vector2.ONE * 5, Vector2.ZERO, Vector2.ONE*3)
-	
-	for x in map.rect.size.x:
-		for y in map.rect.size.y:
-			asserts.is_equal(map.world_to_map(Vector2(x*3,y*3)),Vector2(x,y),"World to Map convertion at " + str(Vector2(x,y)) + " returns scaled vector")
-			asserts.is_equal(map.map_to_world(Vector2(x,y)),Vector2(x*3,y*3),"Map to World convertion at " + str(Vector2(x,y)) + " returns scaled vector")
+func generate_convertion_paramaters(params : Array, default : Array) -> Array:
+	var out := []
+	out.append(params)
+	out.append(default)
+	for i in range(1,params.size()):
+		var new_params := [] + default
+		new_params[i] = Vector2.ONE * 3
+		out.append(new_params)
+	return out
 
-func test_unscaled_transformed_conversions():
-	describe("Unscaled, transformed (3,3) convertions")
-	
-	var map := initialize_full_tilemap(Vector2.ONE * 5, Vector2.ONE * 3)
-	
-	for x in map.rect.size.x:
-		for y in map.rect.size.y:
-			asserts.is_equal(map.world_to_map(Vector2(x + 3,y + 3)),Vector2(x,y),"World to Map convertion at " + str(Vector2(x,y)) + " returns transformed vector")
-			asserts.is_equal(map.map_to_world(Vector2(x,y)),Vector2(x + 3,y + 3),"Map to World convertion at " + str(Vector2(x,y)) + " returns transformed vector")
+func generate_convertion_description(params : Array, default : Array) -> String:
+	var description := "conversions on a " + str(p.size) + " map"
+	for i in params.size():
+		if p[params[i]] != default[i]:
+			description += " with " + params[i]
+	return description
 
-func test_scaled_transformed_conversions():
-	describe("Scaled x3, transformed (3,3) convertions")
-	
-	var map := initialize_full_tilemap(Vector2.ONE * 5, Vector2.ONE *3, Vector2.ONE *3)
-	
+func generate_convertion_map() -> Map:
+	var map := initialize_full_tilemap(p.size, p.cell_transform, p.cell_size)
+	map.tile_map.position = p.object_transform
+	map.tile_map.scale = p.object_size
+	map.tile_map.cell_tile_origin = TileMap.TILE_ORIGIN_CENTER
+	return map
+
+func run_conversion_tests(map : Map):
 	for x in map.rect.size.x:
 		for y in map.rect.size.y:
-			asserts.is_equal(map.world_to_map(Vector2((x+3)*3,(y+3)*3)),Vector2(x,y),"World to Map convertion at " + str(Vector2(x,y)) + " returns scaled, transformed vector")
-			asserts.is_equal(map.map_to_world(Vector2(x,y)),Vector2((x+3)*3,(y+3)*3),"Map to World convertion at " + str(Vector2(x,y)) + " returns scaled, transformed vector")
+			run_conversion_test(map, Vector2(x,y))
+
+func run_conversion_test(map : Map, map_point : Vector2):
+	var world_point := map.tile_map.map_to_world(map_point + map.rect.position) * map.tile_map.scale + map.tile_map.position 
+	world_point += map.tile_map.cell_size/2
+	asserts.is_equal(map.world_to_map(world_point),map_point,"World to Map convertion at " + str(map_point))
+	asserts.is_equal(map.map_to_world(map_point),world_point,"Map to World convertion at " + str(map_point))
 
 func initialize_full_tilemap(size : Vector2, offset : Vector2 = Vector2.ZERO, tile_scale : Vector2 = Vector2.ONE) -> Map:
 	var tilemap := TileMap.new()
