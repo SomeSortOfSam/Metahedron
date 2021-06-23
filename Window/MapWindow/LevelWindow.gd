9b522bcced86124e56262e6cce436b2498d075ca
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-06-23 14:23:18 -0500

    Window

--- Window/MapWindow/LevelWindow.gd
+++ Window/MapWindow/LevelWindow.gd
@@ -5,7 +5,7 @@ var map : Map
 func _ready():
 	resize_window()
 	map = Map.new($TileMap)
-	get_tree().connect("screen_resized",self, "resize_window")
+	get_tree().connect("screen_resized", self, "resize_window")
 
 func resize_window():
 	popup(get_viewport_rect())
---
+++ Window/MapWindow/Movement Window.gd
@@ -0,0 +1,2 @@
+extends WindowDialog
+class_name MovementWindow
