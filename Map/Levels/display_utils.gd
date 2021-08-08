5ac1a3ee0d3a0ed13654c9dd539e7621739e4a34
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-08-07 19:36:45 -0500

    Update display_utils.gd

--- Map/Levels/display_utils.gd
+++ Map/Levels/display_utils.gd
@@ -5,7 +5,7 @@ static func draw_index_rect(item):
 	if (Engine.editor_hint || item.override_in_editor) && tilemap:
 		var rect = get_cell_rect(item,tilemap)
 		rect = apply_index_transformation(item,tilemap,rect)
-		item.draw_rect(rect,item.get_tool_color(),false,get_index_offset(item,tilemap).x)
+		item.draw_rect(rect,item.get_tool_color(),false,get_index_offset(tilemap).x)
 
 static func get_cell_rect(item,tilemap):
 	var cell = tilemap.world_to_map(item.position - item.get_offset())
@@ -13,11 +13,11 @@ static func get_cell_rect(item,tilemap):
 	return Rect2(cell_position,tilemap.cell_size)
 
 static func apply_index_transformation(item,tilemap,rect) -> Rect2:
-	var index_offset = get_index_offset(item,tilemap)
+	var index_offset = get_index_offset(tilemap)
 	rect.position += index_offset * item.get_index()
 	rect.size -= index_offset * item.get_index() * 2
 	return rect
 
-static func get_index_offset(item,tilemap):
+static func get_index_offset(tilemap):
 	return tilemap.cell_size/2/tilemap.get_child_count()
 
