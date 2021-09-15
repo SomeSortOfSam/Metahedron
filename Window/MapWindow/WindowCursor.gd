944427d9af74fffeed6cf23d21779fe6bfabfec8
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-09-15 15:58:13 -0500

    Arrowline Error prevention

--- Window/MapWindow/WindowCursor.gd
+++ Window/MapWindow/WindowCursor.gd
@@ -6,7 +6,7 @@ func _gui_input(event):
 	if "position" in event:
 		var map = get_parent().map
 		cell = MapSpaceConverter.local_to_map(event.position - $TilemapContainer.position,map)
-		if Pathfinder.is_walkable(cell,map):
+		if Pathfinder.is_walkable(cell,map) && Pathfinder.is_cell_in_range(Vector2.ZERO,cell,map.tile_range):
 			$TilemapContainer/ArrowLines.draw_path(cell)
 		else:
 			$TilemapContainer/ArrowLines.clear()
