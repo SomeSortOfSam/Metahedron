767c7dcc8c2d9204e1fca4b882884ed151bd76aa
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-10-28 15:52:58 -0500

    Turn tests implemented

--- Tests/Primary/feature_turns.gd
+++ Tests/Primary/feature_turns.gd
@@ -8,22 +8,83 @@ extends "res://addons/gut/test.gd"
 # Skiping a turn ends a units turn, but can be reversed if other units have not ended their turn
 
 func test_single_attack():
-	pending()
+	#Arrange
+	var turn_manager = TurnManager.new()
+	var person = Person.new(Character.new())
+	turn_manager._on_map_person_added(person)
+	watch_signals(turn_manager)
+	#Act
+	person.emit_signal("has_attacked")
+	#Assert
+	assert_signal_emitted(turn_manager,"turn_ended")
 
 func test_single_move():
-	pending()
+	#Arrange
+	var turn_manager = TurnManager.new()
+	var person = Person.new(Character.new())
+	turn_manager._on_map_person_added(person)
+	watch_signals(turn_manager)
+	#Act
+	person.emit_signal("cell_change",Vector2.ZERO)
+	#Assert
+	assert_signal_not_emitted(turn_manager,"turn_ended")
 
 func test_single_close():
-	pending()
+	#Arrange
+	var turn_manager = TurnManager.new()
+	var person = Person.new(Character.new())
+	turn_manager._on_map_person_added(person)
+	watch_signals(turn_manager)
+	#Act
+	person.emit_signal("has_set_end_turn",true)
+	#Assert
+	assert_signal_emitted(turn_manager,"turn_ended")
 
 func test_single_skip():
-	pending()
+	#Arrange
+	var turn_manager = TurnManager.new()
+	var person = Person.new(Character.new())
+	turn_manager._on_map_person_added(person)
+	watch_signals(turn_manager)
+	#Act
+	person.emit_signal("has_set_end_turn",true)
+	#Assert
+	assert_signal_emitted(turn_manager,"turn_ended")
 
 func test_reversed_close():
-	pending()
+	#Arrange
+	var turn_manager = TurnManager.new()
+	var person = Person.new(Character.new())
+	turn_manager._on_map_person_added(Person.new(Character.new()))
+	turn_manager._on_map_person_added(person)
+	watch_signals(turn_manager)
+	#Act
+	person.emit_signal("has_set_end_turn",true)
+	person.emit_signal("has_set_end_turn",false)
+	#Assert
+	assert_signal_not_emitted(turn_manager,"turn_ended")
 
 func test_reversed_skip():
-	pending()
+	#Arrange
+	var turn_manager = TurnManager.new()
+	var person = Person.new(Character.new())
+	turn_manager._on_map_person_added(Person.new(Character.new()))
+	turn_manager._on_map_person_added(person)
+	watch_signals(turn_manager)
+	#Act
+	person.emit_signal("has_set_end_turn",true)
+	person.emit_signal("has_set_end_turn",false)
+	#Assert
+	assert_signal_not_emitted(turn_manager,"turn_ended")
 
 func test_double_attack():
-	pending()
+	#Arrange
+	var turn_manager = TurnManager.new()
+	var person = Person.new(Character.new())
+	turn_manager._on_map_person_added(Person.new(Character.new()))
+	turn_manager._on_map_person_added(person)
+	watch_signals(turn_manager)
+	#Act
+	person.emit_signal("has_attacked")
+	#Assert
+	assert_signal_not_emitted(turn_manager,"turn_ended")
