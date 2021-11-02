b24bd394bd0caff11f82b46f09b48d5627f0cbeb
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-11-01 22:34:00 -0500

    Quantisised (poorly)

--- Scripts/Primary/Window/WindowMover.gd
+++ Scripts/Primary/Window/WindowMover.gd
@@ -38,21 +38,20 @@ func handle_mouse_event(event : InputEventMouse):
 func check_delta(delta : Vector2) -> Vector2:
 	var viewport_rect := viewport_window.get_rect()
 	var hypotheical := window.get_rect()
-	hypotheical.position += delta
 	
 	delta = clamp_delta(delta,hypotheical,viewport_rect)
-	hypotheical.position += delta
 	
-	#var cell_size := tilemap.cell_size * tilemap.global_scale
-	#delta = quatizize_delta(delta,hypotheical,viewport_rect,cell_size,rect_size.y,tilemap_container.get_rect())
+	var cell_size := tilemap.cell_size * tilemap.global_scale
+	delta = quatizize_delta(delta,hypotheical,viewport_rect,cell_size,rect_size.y,tilemap_container.get_rect())
 	
 	return delta
 
 static func clamp_delta(delta : Vector2, window_rect : Rect2, viewport_rect : Rect2) -> Vector2:
+	window_rect.position += delta
 	if window_rect.position.x < viewport_rect.position.x:
-		delta.x -= window_rect.position.x
+		delta.x += viewport_rect.position.x - window_rect.position.x
 	if window_rect.position.y < viewport_rect.position.y:
-		delta.y -= window_rect.position.y
+		delta.y += viewport_rect.position.y - window_rect.position.y
 	if window_rect.position.x + window_rect.size.x > viewport_rect.position.x + viewport_rect.size.x:
 		delta.x -= (window_rect.position.x + window_rect.size.x)-(viewport_rect.position.x + viewport_rect.size.x)
 	if window_rect.position.y + window_rect.size.y > viewport_rect.position.y + viewport_rect.size.y:
@@ -68,7 +67,16 @@ static func quatizize_delta(delta : Vector2, window_rect : Rect2, viewport_rect 
 	var quantiziation_rect = get_quantiziation_rect(viewport_rect,cell_size,center_cell_distance,\
 	tilemap_container_rect)
 	delta = clamp_delta(delta,center_cell_rect,quantiziation_rect)
+	delta = round_to_nearest_cell(delta,center_cell_rect,quantiziation_rect)
 	return delta
+
+static func round_to_nearest_cell(delta : Vector2, center_cell_rect : Rect2, quantiziation_rect : Rect2) -> Vector2:
+	center_cell_rect.position += delta
+	var pos := center_cell_rect.position - quantiziation_rect.position
+	var cell_pos := pos
+	cell_pos.x = stepify(cell_pos.x,center_cell_rect.size.x)
+	cell_pos.y = stepify(cell_pos.y,center_cell_rect.size.y)
+	return cell_pos - pos
 
 static func get_quantiziation_rect(viewport_rect : Rect2, cell_size : Vector2, \
 center_cell_distance : Vector2, tilemap_container_rect : Rect2) -> Rect2:
@@ -114,34 +122,3 @@ func _on_TopBar_mouse_entered():
 
 func _on_TopBar_mouse_exited():
 	body.color.v -= .1
-
-func _draw():
-	var cell_size = tilemap.cell_size * tilemap.global_scale
-	var veiwport_rect := viewport_window.get_rect()
-	var tilemap_container_rect := tilemap_container.get_rect()
-	var center_cell_distance := get_center_cell_distance(rect_size.y,tilemap_container_rect,\
-	cell_size)
-	var rect := get_quantiziation_rect(veiwport_rect,cell_size,center_cell_distance,\
-	tilemap_container_rect)
-	rect.position -= window.rect_position
-	draw_grid(rect,cell_size,Color.green)
-	var rect2 = Rect2(window.rect_position + center_cell_distance, cell_size)
-	draw_rect(rect2,Color.red)
-	var center_cell_pos : Vector2 = window.rect_position + center_cell_distance + cell_size/2
-	var delta_pos := center_cell_pos + clamp_delta(Vector2.ZERO,rect2,rect)
-	draw_line(center_cell_pos,delta_pos,Color.yellow)
-
-func draw_grid(rect : Rect2, cell_size : Vector2, color : Color):
-	draw_rect(rect,color,false)
-	for x in rect.size.x/cell_size.x:
-		var start := rect.position 
-		start.x += x * cell_size.x
-		var end := start
-		end.y += rect.size.y
-		draw_line(start,end,color)
-	for y in rect.size.y/cell_size.y:
-		var start := rect.position 
-		start.y += y * cell_size.y
-		var end := start
-		end.x += rect.size.x
-		draw_line(start,end,color)
--- Tests/Primary/feature_window_mover.gd
+++ Tests/Primary/feature_window_mover.gd
@@ -1,12 +1,12 @@
 extends "res://addons/gut/test.gd"
 
 func test_out_of_bounds_clamp():
-	var parent_rect := Rect2(Vector2.ZERO,Vector2.ONE*3)
+	var parent_rect := Rect2(Vector2.ONE*3,Vector2.ONE*3)
 	for x in range(-1,4):
 		for y in range(-1,4):
-			var child_rect := Rect2(x,y,1,1)
+			var child_rect := Rect2(x+3,y+3,1,1)
 			child_rect.position += WindowMover.clamp_delta(Vector2.ZERO,child_rect,parent_rect)
-			assert_true(parent_rect.encloses(child_rect),"Clamp works for" + str(x) + "," + str(y))
+			assert_true(parent_rect.encloses(child_rect),"Clamp works for (" + str(x) + "," + str(y) + ")")
 
 func populate_mover(parent : Control) -> WindowMover:
 	var child := Control.new()
