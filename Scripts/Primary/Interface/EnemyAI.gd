0644794a0ac51c9df549cefd7a68db4decc4a9d8
WesleyHill25 <90586574+WesleyHill25@users.noreply.github.com>
2021-11-16 21:04:06 -0800

    Updated EnemyAI's movement
    I did some extra stuff and now the AI is intended to move to the best cell adjacent to the nearest enemy, I need to keep working on the system.

--- Scripts/Primary/Interface/EnemyAI.gd
+++ Scripts/Primary/Interface/EnemyAI.gd
@@ -41,28 +41,62 @@ func get_closest_friendly(cell : Vector2) -> Vector2:
 			closest_cell = friendly_cell
 	return closest_cell
 
-#Returns the closest cell from origin_cell adjacent to target_cell
-func get_closest_adjacent_cell(origin_cell : Vector2, target_cell : Vector2) -> Vector2:
-	var x_dist = target_cell.x - origin_cell.x
-	var y_dist = target_cell.y - origin_cell.y
-	if(abs(x_dist) > abs(y_dist)):
-		if(x_dist < 0):
-			target_cell.x += 1
-		else:
-			target_cell.x -= 1
-	else:
-		if(y_dist < 0):
-			target_cell.y += 1
-		else:
-			target_cell.y -= 1
-	return target_cell
+func get_all_adjacent_cells(cell : Vector2) -> Array:
+	var cells = [cell + Vector2.UP, cell + Vector2.RIGHT, cell + Vector2.LEFT, cell + Vector2.DOWN]
+	return cells
+
+#Returns the number of player controlled units in the same column as cell
+func get_num_friendlies_in_column(cell : Vector2) -> int:
+	var friendlies = get_friendly_units()
+	var count := 0
+	for friendly_cell in friendlies:
+		if friendly_cell.x == cell.x:
+			count += 1
+		print(friendlies[friendly_cell].cell == friendly_cell)
+	return count
+
+#Returns the number of player controlled units in the same row as cell
+func get_num_friendlies_in_row(cell : Vector2) -> int:
+	var friendlies = get_friendly_units()
+	var count := 0
+	for friendly_cell in friendlies:
+		if friendly_cell.y == cell.y:
+			count += 1
+	return count
+
+func determine_cell_score(cell : Vector2) -> int:
+	var score := 0
+	score += get_num_friendlies_in_column(cell) + get_num_friendlies_in_row(cell)
+	return score
+
+func get_all_cells_in_range(person : Person) -> Array:
+	var cells : Array
+	var directions = [Vector2.UP, Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT]
+	for direction in directions:
+		for x in 2:
+			cells.append(direction * x)
+	cells.append(Vector2.ZERO)
+	cells.append_array([Vector2.ONE, -Vector2.ONE])
+	return cells
 
 func start_enemy_turn():
 	var enemies = get_enemies()
 	for cell in enemies:
 		var enemy : Person = enemies[cell]
-		var closest_friendly_cell = get_closest_friendly(cell)
-		var target_cell = get_closest_adjacent_cell(cell, closest_friendly_cell)
+		
+		var target_cells = get_all_cells_in_range(enemy)
+		
+		var cell_scores = [0, 0, 0, 0]
+		var current_cell_index := 0
+		var max_score_index := -1
+		
+		for target in target_cells:
+			cell_scores[current_cell_index] = determine_cell_score(target_cells[0])
+			if cell_scores[current_cell_index] > cell_scores[max_score_index]:
+				max_score_index = current_cell_index
+			current_cell_index += 1
+		
+		var target_cell = target_cells[max_score_index]
 		var delta_cell : Vector2 = target_cell - cell
 		enemy.cell += delta_cell
 		enemy.set_skipped(true)
