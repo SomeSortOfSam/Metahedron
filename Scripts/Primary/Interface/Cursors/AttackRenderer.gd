ba177fdc71235e30ccd176a375ddbb692b98efa5
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-11-22 08:59:59 -0600

    Local basic Attack implemented

--- Scripts/Primary/Interface/Cursors/AttackRenderer.gd
+++ Scripts/Primary/Interface/Cursors/AttackRenderer.gd
@@ -12,10 +12,24 @@ func draw_display(to : Vector2, acceptable : bool):
 	clear()
 	
 	if acceptable and attack:
-		for cell in attack.attack(map, map.center_cell, to):
+		for cell in attack.attack(map.map, map.center_cell, get_closest_direction(to)):
 			set_cellv(MapSpaceConverter.internal_map_to_map(cell,map),0)
 		for display in get_tree().get_nodes_in_group("displays"):
 			display.attack = attack
 
 func _on_map_change(new_map):
 	map = new_map
+
+static func get_closest_direction(to : Vector2) -> Vector2:
+	var directions := [Vector2.UP,Vector2.DOWN,Vector2.LEFT,Vector2.RIGHT]
+	var lowest_distance := 10000
+	var lowest_distance_index := 0
+	
+	for i in directions.size():
+		var distance = to.distance_squared_to(directions[i])
+		if distance < lowest_distance:
+			lowest_distance = distance
+			lowest_distance_index = i
+	
+	return directions[lowest_distance_index]
+	
--- Scripts/Primary/Level/Resources/Attacks/Attack.gd
+++ Scripts/Primary/Level/Resources/Attacks/Attack.gd
@@ -14,25 +14,50 @@ export var time_to_complete : float
 export var projectile : Texture
 export var custom_animation : String
 
-func attack(map : Map, center_cell : Vector2, direction : Vector2) -> Array:
-	var array = []
+func attack(map : Map, center_cell : Vector2, direction : Vector2) -> PoolVector2Array:
+	var array := get_bitmask_delta_array()
+	array = change_deltas_to_direction(array,direction)
 	
-	var image = bitmask.get_data()
+	var i := 0
+	while i < array.size():
+		array[i] += center_cell
+		if !Pathfinder.is_walkable(array[i],map):
+			array.remove(i)
+			i -= 1
+		i += 1
+	
+	return array
+
+func get_bitmask_delta_array() -> PoolVector2Array:
+	var array := PoolVector2Array([])
+	
+	var image := bitmask.get_data()
 	image.lock()
 	
 	var orgin = Vector2((image.get_width() / 2), image.get_height() - 1)
+	var new_orgin_found := false
 	
 	for w in image.get_width():
 		for h in image.get_height():
-			if image.get_pixel(w, h) == Color.red:
-				orgin = Vector2(w, h)
-			elif image.get_pixel(w, h) == Color.blue:
+			if image.get_pixel(w, h).r == 1.0:
+				if !new_orgin_found:
+					orgin = Vector2(w, h)
+					new_orgin_found = true
+				else:
+					return PoolVector2Array([])
+			if image.get_pixel(w, h).b == 1.0:
 				array.append(Vector2(w, h))
 	
 	image.unlock()
 	
 	for i in array.size():
 		array[i] -= orgin
-		array[i] += center_cell
 	
 	return array
+
+func change_deltas_to_direction(delta_array : PoolVector2Array, direction : Vector2) -> PoolVector2Array:
+	var right := Vector2(-direction.y,direction.x)
+	for i in delta_array.size():
+		delta_array[i] = (delta_array[i].y * -direction) + (delta_array[i].x * right)
+	
+	return delta_array
