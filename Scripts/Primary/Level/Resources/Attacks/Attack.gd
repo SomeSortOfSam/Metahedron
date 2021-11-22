b9b0b4e6efda00350a924b11aa529bbc728ffb35
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-11-22 15:34:16 -0600

    Attack null behaviour

--- Scripts/Primary/Level/Resources/Attacks/Attack.gd
+++ Scripts/Primary/Level/Resources/Attacks/Attack.gd
@@ -7,7 +7,7 @@ export var description : String = "More defualt text!"
 
 export var friendlyFire : bool = false
 
-export var bitmask : Texture = load("res://Assets/Attack Bitmaps/default.png")
+export var bitmask : Texture
 export var distance : int
 
 export var time_to_complete : float
@@ -55,34 +55,39 @@ func get_realative_center(window, source_window) -> Vector2:
 	return out
 
 func get_bitmask_delta_array() -> PoolVector2Array:
-	var array := PoolVector2Array([])
+	var out := PoolVector2Array([])
 	
-	var image := bitmask.get_data()
-	image.lock()
-	
-	var orgin = Vector2((image.get_width() / 2), image.get_height() - 1)
-	var new_orgin_found := false
-	
-	for w in image.get_width():
-		for h in image.get_height():
-			if image.get_pixel(w, h).r == 1.0:
-				if !new_orgin_found:
-					orgin = Vector2(w, h)
-					new_orgin_found = true
-				else:
-					return PoolVector2Array([])
-			if image.get_pixel(w, h).b == 1.0:
-				array.append(Vector2(w, h))
-	
-	image.unlock()
-	
-	for i in array.size():
-		array[i] -= orgin
-	
-	return array
+	if bitmask:
+		var image := bitmask.get_data()
+		image.lock()
+		
+		var orgin = Vector2((image.get_width() / 2), image.get_height() - 1)
+		var new_orgin_found := false
+		
+		for w in image.get_width():
+			for h in image.get_height():
+				if image.get_pixel(w, h).r == 1.0:
+					if !new_orgin_found:
+						orgin = Vector2(w, h)
+						new_orgin_found = true
+					else:
+						return PoolVector2Array([])
+				if image.get_pixel(w, h).b == 1.0:
+					out.append(Vector2(w, h))
+		
+		image.unlock()
+		
+		for i in out.size():
+			out[i] -= orgin
+	else:
+		out = get_defualt_bitmask_deltas()
+	return out
 
 func change_deltas_to_direction(delta_array : PoolVector2Array, direction : Vector2) -> PoolVector2Array:
 	var right := Vector2(-direction.y,direction.x)
 	for i in delta_array.size():
 		delta_array[i] = ((delta_array[i].y + distance) * -direction) + (delta_array[i].x * right)
 	return delta_array
+
+func get_defualt_bitmask_deltas() -> PoolVector2Array:
+	return PoolVector2Array([Vector2.UP])
