a0f5d3f5b36b8da69bafe100d24dd87c15ee7cc1
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-11-28 19:29:01 -0600

    Attack Preview

--- Scripts/Primary/Interface/Cursors/AttackRenderer.gd
+++ Scripts/Primary/Interface/Cursors/AttackRenderer.gd
@@ -2,6 +2,7 @@ extends TileMap
 class_name AttackRenderer
 
 const GROUP_NAME := "combat_displays"
+const DIRECTIONS := [Vector2.UP,Vector2.DOWN,Vector2.LEFT,Vector2.RIGHT]
 
 var attack : Attack
 var map : ReferenceMap
@@ -11,14 +12,20 @@ var attack_center : Vector2
 func _ready():
 	add_to_group(GROUP_NAME)
 
-
 func draw_display(to : Vector2, acceptable : bool):
 	clear()
 	
-	if acceptable and attack:
-		for cell in attack.attack(map.map, attack_center, get_closest_direction(to)):
-			if Pathfinder.is_cell_in_range(map.center_cell,cell,map.tile_range):
-				set_cellv(MapSpaceConverter.internal_map_to_map(cell,map),0)
+	var chosen_direction := Vector2.ZERO
+	if acceptable: 
+		 chosen_direction = get_closest_direction(to)
+	
+	if acceptable && attack:
+		for direction in DIRECTIONS:
+			for cell in attack.attack(map.map, attack_center, direction):
+				if Pathfinder.is_cell_in_range(map.center_cell,cell,map.tile_range):
+					cell = MapSpaceConverter.internal_map_to_map(cell,map)
+					var tile := get_cellv(cell)
+					set_cellv(cell,0 if chosen_direction == direction || tile == 0 else 1)
 		for display in get_tree().get_nodes_in_group(GROUP_NAME):
 			display.attack = attack
 
@@ -27,15 +34,15 @@ func _on_map_change(new_map):
 
 static func get_closest_direction(to : Vector2) -> Vector2:
 	to = to.normalized()
-	var directions := [Vector2.UP,Vector2.DOWN,Vector2.LEFT,Vector2.RIGHT]
+	
 	var lowest_distance := 10000
 	var lowest_distance_index := 0
 	
-	for i in directions.size():
-		var distance = to.distance_squared_to(directions[i])
+	for i in DIRECTIONS.size():
+		var distance = to.distance_squared_to(DIRECTIONS[i])
 		if distance < lowest_distance:
 			lowest_distance = distance
 			lowest_distance_index = i
 	
-	return directions[lowest_distance_index]
+	return DIRECTIONS[lowest_distance_index]
 	
