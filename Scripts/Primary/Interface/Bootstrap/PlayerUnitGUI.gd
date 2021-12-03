fc92ead7b3e373780daab057506db1c398f91409
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-12-03 13:29:29 -0600

    Player Gui Animations

--- Scripts/Primary/Interface/Bootstrap/PlayerUnitGUI.gd
+++ Scripts/Primary/Interface/Bootstrap/PlayerUnitGUI.gd
@@ -10,6 +10,7 @@ onready var moved : TextureRect = $AspectRatioContainer/EndTurn/Control2/Moved
 onready var attacked : TextureRect = $AspectRatioContainer/EndTurn/Control2/Attacked
 onready var outline : ColorRect = $Outline
 onready var body : ColorRect = $Body/BodyColor
+onready var animator : AnimationPlayer = $Body/AnimationPlayer
 
 export var new_turn_outline_color : Color
 export var has_moved_outline_color : Color
@@ -18,11 +19,15 @@ export var new_turn_body_color : Color
 export var has_set_end_turn_body_color : Color
 export var has_attacked_body_color : Color
 
+var base_health : int
+
 signal request_end_turn()
 
 func subscribe(person : Person):
+	animator.play_backwards("Died")
 	call_deferred("_on_person_new_turn", person)
 	populate_textures(person)
+	base_health = person.health
 	var _connection = connect("request_end_turn",person,"_on_PlayerUnitGUI_requesting_end_turn")
 	_connection = connect("pressed", person, "open_window")
 	_connection = person.connect("move",self,"_on_person_move")
@@ -47,17 +52,27 @@ func _on_person_attack(_direction,_attack):
 func _on_person_end_turn():
 	body.color = has_set_end_turn_body_color
 
-func _on_person_new_turn(person):
+func _on_person_new_turn(person : Person):
 	portrait.texture = person.character.animations.get_frame("Idle",0)
 	outline.color = new_turn_outline_color
 	body.color = new_turn_body_color
 	attacked.modulate = Color.red
 	moved.modulate = Color.yellow
 
-func _on_person_hurt(person):
+func _on_person_hurt(person : Person):
 	portrait.texture = person.character.animations.get_frame("Hurt",0)
+	animator.play("Hurt")
+	body.set_margins_preset(PRESET_WIDE)
+	body.set_anchor(MARGIN_RIGHT,0)
+	body.margin_right = (body.margin_right/base_health) * person.health
+	if (person.health == 1):
+		body.color = Color.red
 
 func _on_person_died():
+	var _connection = animator.connect("animation_finished",self,"_on_died_animation_ended",[],CONNECT_ONESHOT)
+	animator.play("Died")
+
+func _on_died_animation_ended(_name):
 	queue_free()
 
 func _on_EndTurn_pressed():
